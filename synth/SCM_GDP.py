7a5df238202278f8c737fc58bbc3779f8fd6e7ec
Ricardo Duque Gabriel <ricardo.gabriel@uni-bonn.de>
2019-02-03 17:41:08 +0100

    Adjust SCM_GDP.py

--- synth/SCM_GDP.py
+++ synth/SCM_GDP.py
@@ -2,15 +2,49 @@
 Created on Sun Feb  3 14:08:09 2019
 @author: Ricardo Duque Gabriel
 
-Code for the creation of a SCG for GDP series of the European Monetary Union 
-Countries using countries outside the MU to be our donor pool
+Code for the creation of a SCG for Australian GDP series  
+using 34 OECD countries to be our donor pool
 """
 
 import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
-import numpy as np
-from scipy.optimize import fmin_slsqp, minimize
-import matplotlib.pyplot as plt
+from run import synth_tables
 
-some_dataframe = pd.read_excel('Dataset_TermPaper.xlsx', header=1)
+
+#import excel dataset to panda dataframe
+df = pd.read_excel('Dataset_TermPaper.xlsx', header=0).iloc[:3360]
+
+#define control units
+control_units = list(set(df["code"]))
+control_units.sort()
+#leaving Australia out (code==AUS) 
+control_units = control_units[1:]
+
+#transform variables 'year' and 'quarter' to one integer time variable
+#adjust in excel next time
+x=1
+for i in range(0,3360):
+    df.year[i]=x
+    x=x+1
+    if x==97:
+        x=1
+
+df = df[df.gdp_s.notnull()]
+df.drop(["unemp", "peg"], axis = 1, inplace = True) 
+
+#define which predictors to use
+predictors = [  "privcons_s", "govcons_s", "inv_s", "population", "debt"
+             ]
+
+#create synth tables
+synth_tables(  df,
+               predictors,
+               "AUS",
+               control_units,
+               "code",
+               "gdp_s",
+               "year",
+               [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+               [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+               [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+               )
+
